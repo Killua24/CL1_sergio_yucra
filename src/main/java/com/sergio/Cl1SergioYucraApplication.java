6e841f60bc5a194513ed74572d63af6a96d306d7
sergio <ulquiora20@gmail.com>
2023-09-29 22:01:48 -0500

    Empezo el examen

---
+++ src/main/java/com/sergio/Cl1SergioYucraApplication.java
@@ -0,0 +1,13 @@
+package com.sergio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1SergioYucraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1SergioYucraApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sergio/model/Celular.java
@@ -0,0 +1,5 @@
+package com.sergio.model;
+
+public class Celular {
+
+}
